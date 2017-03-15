3cc4bf61095038ffeba1bea44d95512e5ff220c9
stufengrau <stufengrau@gmail.com>
2017-03-15 15:06:50 +0100

    feat: Search Google Books for a given search term

---
+++ BookCase/BookSearchViewController.swift
@@ -0,0 +1,36 @@
+//
+//  BookSearchViewController.swift
+//  BookCase
+//
+//  Created by heike on 15/03/2017.
+//  Copyright © 2017 stufengrau. All rights reserved.
+//
+
+import UIKit
+
+class BookSearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func searchGoogleBooks(_ sender: UIButton) {
+        let searchTerm = "vim"
+        GoogleBooksAPI.shared.searchGoogleBooks(searchTerm) { (result) in
+            switch result {
+            case .success:
+                debugPrint("Google Books Search was successful")
+            case .failure:
+                debugPrint("Something went wrong with the Google Books Search")
+            }
+        }
+    }
+
+}
+
--- BookCase/GoogleBooksAPI.swift
+++ BookCase/GoogleBooksAPI.swift
@@ -1,6 +1,6 @@
 //
 //  GoogleBooksAPI.swift
-//  BookLibrary
+//  BookCase
 //
 //  Created by heike on 14/03/2017.
 //  Copyright © 2017 stufengrau. All rights reserved.
@@ -8,7 +8,97 @@
 
 import Foundation
 
+enum SearchGoogleBooksResult {
+    case success
+    case failure
+}
 
 class GoogleBooksAPI {
     
+    // MARK: Properties
+    private var session = URLSession.shared
+    
+    // Singleton
+    static let shared = GoogleBooksAPI()
+    private init() {}
+    
+    // MARK: Network Requests
+    
+    // Search Google Books 
+    func searchGoogleBooks(_ searchTerm: String, completionHandler: @escaping (SearchGoogleBooksResult) -> Void) {
+        
+        let methodParameters = [
+            GoogleBooksParameterKeys.Query: searchTerm,
+            GoogleBooksParameterKeys.Results: GoogleBooksParameterValues.maxResults,
+            GoogleBooksParameterKeys.APIKey: GoogleBooksAPIKey.APIKey
+        ]
+        
+        let request = NSMutableURLRequest(url: googleBooksURLFromParameters(methodParameters))
+        
+        session.dataTask(with: request as URLRequest) { data, response, error in
+            
+            guard let parsedResult = self.getResult(data: data, response: response, error: error) else {
+                completionHandler(.failure)
+                return
+            }
+            
+            // does the data contain a key "items"?
+            guard let items = parsedResult[GoogleBooksAPI.GoogleBooksResponseKeys.Items] as? [[String:AnyObject]] else {
+                completionHandler(.failure)
+                return
+            }
+            
+            debugPrint("Items: \(items)")
+            
+            completionHandler(.success)
+            
+            }.resume()
+        
+    }
+    
+    
+    // Create Google Books Search URL from Parameters
+    private func googleBooksURLFromParameters(_ parameters: [String:String]) -> URL {
+        
+        var components = URLComponents()
+        components.scheme = GoogleBooksURL.APIScheme
+        components.host = GoogleBooksURL.APIHost
+        components.path = GoogleBooksURL.APIPath
+        components.queryItems = [URLQueryItem]()
+        
+        for (key, value) in parameters {
+            let queryItem = URLQueryItem(name: key, value: "\(value)")
+            components.queryItems!.append(queryItem)
+        }
+        
+        return components.url!
+    }
+    
+    // Given raw JSON, return a usable Foundation object
+    private func convertData(_ data: Data?) -> AnyObject? {
+        
+        guard let data = data else {
+            return nil
+        }
+        
+        return try? JSONSerialization.jsonObject(with: data, options: .allowFragments) as AnyObject
+    }
+    
+    // Return the parsed Result if no errors occured
+    private func getResult(data: Data?, response: URLResponse?, error: Error?) -> [String: AnyObject]? {
+        
+        guard error == nil else {
+            return nil
+        }
+        
+        guard let statusCode = (response as? HTTPURLResponse)?.statusCode, statusCode >= 200 && statusCode <= 299 else {
+            return nil
+        }
+        
+        guard let parsedResult = convertData(data) as? [String: AnyObject] else {
+            return nil
+        }
+        
+        return parsedResult
+    }
 }
