4bc36480a2ba4145b008dfd889c82e34665c83ab
stufengrau <stufengrau@gmail.com>
2017-03-17 18:53:14 +0100

    feat: Display book search results in a table view

---
+++ BookCase/BookOverviewTableViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  BookOverviewTableViewCell.swift
+//  BookCase
+//
+//  Created by heike on 16/03/2017.
+//  Copyright © 2017 stufengrau. All rights reserved.
+//
+
+import UIKit
+
+class BookOverviewTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var bookThumbnail: UIImageView!
+    @IBOutlet weak var title: UILabel!
+    @IBOutlet weak var authors: UILabel!
+    @IBOutlet weak var publisher: UILabel!
+
+}
--- BookCase/BookSearchViewController.swift
+++ BookCase/BookSearchViewController.swift
@@ -11,6 +11,7 @@ import UIKit
 class BookSearchViewController: UIViewController, UISearchBarDelegate {
 
     @IBOutlet weak var googleBooksSearchBar: UISearchBar!
+    @IBOutlet weak var searchResultTableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -35,6 +36,9 @@ class BookSearchViewController: UIViewController, UISearchBarDelegate {
             case .success:
                 debugPrint("Google Books Search was successful:")
                 dump(Book.shared.bookData)
+                DispatchQueue.main.async {
+                    self.searchResultTableView.reloadData()
+                }
             case .nothingFound:
                 debugPrint("Nothing found for: \(searchTerm)")
             case .failure:
@@ -44,6 +48,40 @@ class BookSearchViewController: UIViewController, UISearchBarDelegate {
         
     }
 
+}
+
+extension BookSearchViewController: UITableViewDataSource, UITableViewDelegate {
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return Book.shared.bookData.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cellIdentifier = "BookOverviewCell"
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath) as! BookOverviewTableViewCell
+        
+        if let coverURL = Book.shared.bookData[indexPath.row].coverURL {
+            GoogleBooksAPI.shared.getBookImage(for: coverURL) { (data) in
+                if let data = data {
+                    DispatchQueue.main.async {
+                        cell.bookThumbnail.image = UIImage(data: data)
+                    }
+                }
+            }
+        }
+
+        cell.title.text = Book.shared.bookData[indexPath.row].title
+        cell.publisher.text = Book.shared.bookData[indexPath.row].publisher
+        cell.authors.text = Book.shared.bookData[indexPath.row].authors.joined(separator: ", ")
+        
+        return cell
+    }
+    
 }
 
 
