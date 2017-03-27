f4e63fc60ca0522f09d99e6038ac9062c58869aa
stufengrau <stufengrau@gmail.com>
2017-03-27 19:33:17 +0200

    fix: Adjust Book Entity Attributes
    to comply to Book Protocol and BookInformation struct

--- BookCase/BookCoreDataProperties.swift
+++ BookCase/BookCoreDataProperties.swift
@@ -8,22 +8,24 @@
 
 import Foundation
 import CoreData
+import UIKit
 
 
 extension BookCoreData {
-
+    
     @nonobjc public class func fetchRequest() -> NSFetchRequest<BookCoreData> {
         return NSFetchRequest<BookCoreData>(entityName: "Book");
     }
-
+    
     @NSManaged public var title: String
     @NSManaged public var subtitle: String?
+    @NSManaged public var authors: [String]
     @NSManaged public var publisher: String?
-    @NSManaged public var authors: NSObject?
-    @NSManaged public var publishedDate: NSObject?
-    @NSManaged public var pages: Int32
-    @NSManaged public var coverURL: String?
+    @NSManaged public var publishedDateAttr: Date?
+    @NSManaged public var publishedDateTypeAttr: String?
+    @NSManaged public var pagesAttr: Int16
     @NSManaged public var googleBookURL: String?
-    @NSManaged public var coverImage: Data?
-
+    @NSManaged public var coverURL: String?
+    @NSManaged public var coverImage: UIImage?
+    
 }
