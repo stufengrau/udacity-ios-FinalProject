c6b35e28cc7c4dd24b4f441e2a97328cb40723fe
stufengrau <stufengrau@gmail.com>
2017-03-28 10:29:31 +0200

    feat: Fetch saved CoreData Books and display in Book List VC

--- BookCase/BookListViewController.swift
+++ BookCase/BookListViewController.swift
@@ -7,10 +7,33 @@
 //
 
 import UIKit
+import CoreData
 
 class BookListViewController: UIViewController {
     
     @IBOutlet weak var tableView: UITableView!
+    
+    var stack: CoreDataStack {
+        let delegate = UIApplication.shared.delegate as! AppDelegate
+        return delegate.stack
+    }
+    
+    var fetchRequest: NSFetchRequest<NSFetchRequestResult>!
+    
+    var fetchedResultsController : NSFetchedResultsController<NSFetchRequestResult>? {
+        didSet {
+            // Whenever the frc changes, we execute the search and reload the data
+            fetchedResultsController?.delegate = self
+            executeSearch()
+            tableView.reloadData()
+        }
+    }
+    
+    func executeSearch() {
+        if let fc = fetchedResultsController {
+            try? fc.performFetch()
+        }
+    }
     
     // MARK: - View Lifecycle
     override func viewDidLoad() {
@@ -21,6 +44,11 @@ class BookListViewController: UIViewController {
         // Self-Sizing Table View Cells
         tableView.rowHeight = UITableViewAutomaticDimension;
         tableView.estimatedRowHeight = 105.0;
+        
+        fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "Book")
+        fetchRequest.sortDescriptors = [NSSortDescriptor(key: "title", ascending: true)]
+        
+        fetchedResultsController = NSFetchedResultsController(fetchRequest: fetchRequest, managedObjectContext: stack.context, sectionNameKeyPath: nil, cacheName: nil)
         
     }
     
@@ -45,7 +73,8 @@ extension BookListViewController: UITableViewDataSource, UITableViewDelegate {
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 3
+        guard let section = fetchedResultsController?.sections?[section] else { return 0 }
+        return section.numberOfObjects
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -53,7 +82,7 @@ extension BookListViewController: UITableViewDataSource, UITableViewDelegate {
         let cellIdentifier = "BookOverviewCell"
         let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath) as! BookOverviewTableViewCell
         
-        cell.configureCell(book: BookImageCaching(bookInformation: BookInformation.from(json: ["title" : "Test" as AnyObject])!))
+        cell.configureCell(book: fetchedResultsController?.object(at: indexPath) as! Book)
         
         return cell
     }
@@ -61,8 +90,58 @@ extension BookListViewController: UITableViewDataSource, UITableViewDelegate {
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         let detailVC = self.storyboard?.instantiateViewController(withIdentifier: "BookDetailView") as! BookDetailTableViewController
         detailVC.detailViewState = DetailViewState.Share
-        detailVC.book = BookImageCaching(bookInformation: BookInformation.from(json: ["title" : "Test" as AnyObject])!)
+        detailVC.book = fetchedResultsController?.object(at: indexPath) as! Book
         self.navigationController?.pushViewController(detailVC, animated: true)
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            
+            stack.context.delete(fetchedResultsController?.object(at: indexPath) as! NSManagedObject)
+            stack.save()
+
+        }
+    }
+    
+    
+}
+
+extension BookListViewController: NSFetchedResultsControllerDelegate {
+    func controllerWillChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
+        tableView.beginUpdates()
+    }
+    
+//    func controller(_ controller: NSFetchedResultsController<NSFetchRequestResult>, didChange sectionInfo: NSFetchedResultsSectionInfo, atSectionIndex sectionIndex: Int, for type: NSFetchedResultsChangeType) {
+//        
+//        let set = IndexSet(integer: sectionIndex)
+//        
+//        switch (type) {
+//        case .insert:
+//            tableView.insertSections(set, with: .fade)
+//        case .delete:
+//            tableView.deleteSections(set, with: .fade)
+//        default:
+//            // irrelevant in our case
+//            break
+//        }
+//    }
+    
+    func controller(_ controller: NSFetchedResultsController<NSFetchRequestResult>, didChange anObject: Any, at indexPath: IndexPath?, for type: NSFetchedResultsChangeType, newIndexPath: IndexPath?) {
+        
+        switch(type) {
+        case .insert:
+            tableView.insertRows(at: [newIndexPath!], with: .fade)
+        case .delete:
+            tableView.deleteRows(at: [indexPath!], with: .fade)
+        case .update:
+            tableView.reloadRows(at: [indexPath!], with: .fade)
+        case .move:
+            assertionFailure("move")
+        }
+    }
+    
+    func controllerDidChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
+        tableView.endUpdates()
     }
     
 }
