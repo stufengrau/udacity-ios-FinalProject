a34a05074e6a32510759bb78ad596fc009928825
stufengrau <stufengrau@gmail.com>
2017-04-03 14:21:19 +0200

    fix: Insertions/Deletions in Core Data while searching in Book List

--- BookCase/BookListViewController.swift
+++ BookCase/BookListViewController.swift
@@ -22,7 +22,7 @@ class BookListViewController: UIViewController {
         return delegate.stack
     }
     
-    private var fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "Book")
+    fileprivate var fetchRequest: NSFetchRequest<NSFetchRequestResult>!
     
     fileprivate var fetchedResultsController: NSFetchedResultsController<NSFetchRequestResult>? {
         didSet {
@@ -35,9 +35,8 @@ class BookListViewController: UIViewController {
     
     private let sortByKey = "Sort by"
     
-    private var searchBar: UISearchBar!
-    fileprivate var notSearching = true
-    fileprivate var filteredBooks: [Book]? = nil
+    fileprivate var searchBar: UISearchBar!
+    fileprivate var searching = false
     
     fileprivate var bookListIsEmpty: Bool! {
         didSet {
@@ -73,6 +72,9 @@ class BookListViewController: UIViewController {
         
         // Sort results by saved segmented control state
         booksSortedBy.selectedSegmentIndex = UserDefaults.standard.integer(forKey: sortByKey)
+        
+        fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "Book")
+        fetchRequest.fetchBatchSize = 20
         sortFetchedResultsBy(selectedSegmentIndex: booksSortedBy.selectedSegmentIndex)
         
         // Configure Search Bar
@@ -119,49 +121,62 @@ class BookListViewController: UIViewController {
         }
     }
     
-
-    
-    /*
-     // MARK: - Navigation
-     
-     // In a storyboard-based application, you will often want to do a little preparation before navigation
-     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-     // Get the new view controller using segue.destinationViewController.
-     // Pass the selected object to the new view controller.
-     }
-     */
-    
 }
 
 // MARK: - Extensions
 extension BookListViewController: UISearchBarDelegate {
+    
     // MARK: UISearchBar Delegate
+    
+    /*  First I tried to filter the objects returned by the fetchedResultsController:
+     
+     filteredBooks = fetchedResultsController?.fetchedObjects as? [Book]
+     
+     if searchText == "" {
+     filteredBooks = fetchedResultsController?.fetchedObjects as? [Book]
+     } else {
+     filteredBooks = fetchedResultsController?.fetchedObjects?.filter() {
+     return searchPredicate!.evaluate(with: $0)
+     } as! [Book]?
+     }
+     tableView.reloadData()
+     
+     The filtered objects were saved in an Array "filteredBooks"
+     TableView functions used the "filteredBooks" Array while in search mode
+     This worked fine, except for Insertions and Deletions of Objects in CoreData
+     
+     The following solutions works fine but I believe performance wise this is not
+     the best way to go for larger datasets. Have to read more about profiling and
+     Core Data in general to find a better implementation.
+     */
     func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
         
-        notSearching = false
+        searching = true
+        booksSortedBy.isEnabled = false
+        
+        fetchRequest.sortDescriptors = [NSSortDescriptor(key: "title", ascending: true)]
         
         let searchPredicate = NSPredicate(format: "(title contains[c] $text) OR (authors contains[c] $text) OR (publisher contains[c] $text) OR (isbn contains[c] $text)").withSubstitutionVariables(["text" : searchText])
-        
-        if searchText == "" {
-            filteredBooks = fetchedResultsController?.fetchedObjects as? [Book]
+        if searchText.isEmpty {
+            fetchRequest.predicate = nil
         } else {
-            filteredBooks = fetchedResultsController?.fetchedObjects?.filter() {
-                return searchPredicate.evaluate(with: $0)
-                } as! [Book]?
+            fetchRequest.predicate = searchPredicate
         }
-        tableView.reloadData()
+        
+        fetchedResultsController = NSFetchedResultsController(fetchRequest: fetchRequest, managedObjectContext: stack.context, sectionNameKeyPath: nil, cacheName: nil)
+        
     }
     
     func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
         
-        notSearching = true
-        
         searchBar.resignFirstResponder()
         searchBar.text = nil
         
-        sortFetchedResultsBy(selectedSegmentIndex: booksSortedBy.selectedSegmentIndex)
-        tableView.reloadData()
+        searching = false
+        booksSortedBy.isEnabled = true
         
+        fetchRequest.predicate = nil
+        sortFetchedResultsBy(selectedSegmentIndex: booksSortedBy.selectedSegmentIndex)
         tableView.setContentOffset(CGPoint(x: 0, y: searchBar.frame.height), animated: true)
         
     }
@@ -184,19 +199,18 @@ extension BookListViewController: UITableViewDataSource, UITableViewDelegate {
     
     // MARK: Table view data source
     func numberOfSections(in tableView: UITableView) -> Int {
-        guard notSearching else { return 1 }
+        guard !searching else { return 1 }
         guard let sections = fetchedResultsController?.sections else { return 0 }
         return sections.count
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        guard notSearching else { return filteredBooks?.count ?? 0 }
         guard let sectionInfo = fetchedResultsController?.sections?[section] else { fatalError("Unexpected Section") }
         return sectionInfo.numberOfObjects
     }
     
     func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-        guard notSearching else { return nil }
+        guard !searching else { return nil }
         guard let sectionInfo = fetchedResultsController?.sections?[section] else { fatalError("Unexpected Section") }
         return sectionInfo.name.isEmpty ? "Unknown" : sectionInfo.name
     }
@@ -206,11 +220,7 @@ extension BookListViewController: UITableViewDataSource, UITableViewDelegate {
         let cellIdentifier = "BookOverviewCell"
         let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath) as! BookOverviewTableViewCell
         
-        if notSearching {
-            cell.configureCell(book: fetchedResultsController?.object(at: indexPath) as! Book)
-        } else {
-            cell.configureCell(book: filteredBooks![indexPath.row])
-        }
+        cell.configureCell(book: fetchedResultsController?.object(at: indexPath) as! Book)
         
         return cell
     }
@@ -218,11 +228,7 @@ extension BookListViewController: UITableViewDataSource, UITableViewDelegate {
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         let detailVC = self.storyboard?.instantiateViewController(withIdentifier: "BookDetailView") as! BookDetailTableViewController
         detailVC.detailViewState = DetailViewState.ShareBook
-        if notSearching {
-            detailVC.book = fetchedResultsController?.object(at: indexPath) as! Book
-        } else {
-            detailVC.book = filteredBooks![indexPath.row]
-        }
+        detailVC.book = fetchedResultsController?.object(at: indexPath) as! Book
         self.navigationController?.pushViewController(detailVC, animated: true)
     }
     
@@ -232,8 +238,6 @@ extension BookListViewController: UITableViewDataSource, UITableViewDelegate {
             stack.save()
         }
     }
-    
-    
 }
 
 extension BookListViewController: NSFetchedResultsControllerDelegate {
@@ -274,7 +278,7 @@ extension BookListViewController: NSFetchedResultsControllerDelegate {
     
     func controllerDidChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
         tableView.endUpdates()
-        bookListIsEmpty = !(tableView.visibleCells.count > 0)
+        bookListIsEmpty = !(tableView.visibleCells.count > 0) && !searching
     }
     
 }
